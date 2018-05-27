6f2855afcef26c69ae6a8f20adfa19a394b101e9
carlos <cramallo65@gmail.com>
2018-05-27 15:08:15 -0300

    graficos1 e incrementador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
-
 //Rutas
 import {APP_ROUTES} from './app.routes';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import {PagesModule} from './pages/pages.module';
-
-
 
 @NgModule({
   declarations: [
--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output,ViewChild,ElementRef,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'incrementador',
@@ -9,7 +9,8 @@ export class IncrementadorComponent implements OnInit {
 
   @Input()leyenda:string="";
   @Input()progress:number=50;
-  @Output()cambioValor:EventEmitter<number>=new EventEmitter();  
+  @Output()cambioValor:EventEmitter<number>=new EventEmitter();
+  @ViewChild('txtProgress') txtProgress:ElementRef;
 
   constructor() { }
 
@@ -17,12 +18,31 @@ export class IncrementadorComponent implements OnInit {
   }
 
   cambiarValor(val:number){
-    //this.progress+=val;
+    if(this.progress>=100 && val>0){
+      this.progress=100;
+      return;
+    }
+    if(this.progress<=0 && val<0){
+      this.progress=0;
+      return;
+    }
+    this.progress=this.progress+val;
+    this.txtProgress.nativeElement.focus();
     this.cambioValor.emit(this.progress);
   }
 
   onChange(newNumber:number){
-    this.cambiarValor(this.progress);
+    //let elemHTML:any=document.getElementsByName('progreso')[0];
+    if(newNumber>=100){
+      this.progress=100;
+    }else if(newNumber<=0){
+      this.progress=0;
+    }else{
+      this.progress=newNumber;
+    }
+    //elemHTML.value=Number(this.progress);
+    this.txtProgress.nativeElement.value=this.progress;
+    this.cambioValor.emit(this.progress);
   }
 
 }
